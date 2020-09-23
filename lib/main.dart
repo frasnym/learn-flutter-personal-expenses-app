64a4ac9b6bd4f2a2d6adfac57c05b2d92ff3c086
Frastyawan Nym <frastyawan.nym@gmail.com>
2020-09-23 14:16:38 +0800

    text styling

--- lib/main.dart
+++ lib/main.dart
@@ -56,28 +56,37 @@ class MyHomePage extends StatelessWidget {
           Column(
             children: transactions.map((tx) {
               return Card(
-                child: Row(children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      horizontal: 10,
-                      vertical: 15,
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2,
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        tx.amount.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
                       ),
                     ),
-                    padding: EdgeInsets.all(10),
-                    child: Text(tx.amount.toString()),
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Text(tx.title),
-                      Text(tx.date.toString()),
-                    ],
-                  )
-                ],),
+                    Column(
+                      children: <Widget>[
+                        Text(tx.title),
+                        Text(tx.date.toString()),
+                      ],
+                    )
+                  ],
+                ),
               );
             }).toList(),
           ),

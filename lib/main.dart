22dc624617c733afad860dcfd22b35cd42c5c333
Frastyawan Nym <frastyawan.nym@gmail.com>
2020-09-23 14:10:14 +0800

    styling container

--- lib/main.dart
+++ lib/main.dart
@@ -58,6 +58,17 @@ class MyHomePage extends StatelessWidget {
               return Card(
                 child: Row(children: <Widget>[
                   Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(10),
                     child: Text(tx.amount.toString()),
                   ),
                   Column(
